8417daa61784860097db62675fb702192711872e
James Thorne <james@jthorne.co.uk>
2016-10-26 16:10:01 +0100

    added get data to setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,3 +4,7 @@ module load apps/python/anaconda3-2.5.0
 cd ..
 
 conda create -n factchecking python=3.5 numpy scikit-learn
+
+
+wget http://nlp.stanford.edu/software/sempre/wikitable/WikiTableQuestions-1.0.2.zip
+unzip WikiTableQuestions-1.0.2.zip
