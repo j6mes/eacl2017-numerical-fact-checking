162bf01c3b84246dbadac9930dd1421eff0e524c
James Thorne <james@jthorne.co.uk>
2016-11-16 17:11:54 +0000

    bag of words classifier

--- src/distant_supervision/utterance_detection.py
+++ src/distant_supervision/utterance_detection.py
@@ -1,6 +1,10 @@
 import sys
+from itertools import groupby
+from operator import itemgetter
+
 from fuzzywuzzy import fuzz, process
 
+from distant_supervision.Match import Match
 from distant_supervision.stop_words import StopWords
 from distant_supervision.strategy.relation_matching import exact_or_fuzzy_match_no_stopwords, stanford_normaliser
 from stanford.corenlpy import SharedPipeline, Annotation, CoreAnnotations, CorefChainAnnotation, Integer, \
@@ -36,6 +40,8 @@ def find_utterances_for_tuple(lines, obj, relation_match_strategy=exact_or_fuzzy
         incoref = False
         tokens = []
 
+        entity_positions = []
+        coref_entity_positions = []
         for i in range(sentence.get(CoreAnnotations.TokensAnnotation).size()):
             corelabel = sentence.get(CoreAnnotations.TokensAnnotation).get(i)
             #print(corelabel.get(CoreAnnotations.TextAnnotation))
@@ -49,15 +55,20 @@ def find_utterances_for_tuple(lines, obj, relation_match_strategy=exact_or_fuzzy
                     break
 
             if coref:
+                coref_entity_positions.append(i)
                 if not incoref:
                     tokens.append(obj['entity'])
                     incoref = True
             else:
+                if fuzz.token_set_ratio(obj['entity'], corelabel.get(CoreAnnotations.TextAnnotation)) > 85 or len(
+                        set(obj['entity'].strip().split()).intersection({corelabel.get(CoreAnnotations.TextAnnotation)})) > 0:
+                    entity_positions.append(i)
+
                 incoref = False
                 tokens.append(corelabel.get(CoreAnnotations.TextAnnotation))
 
         if relation_match_strategy(tokens, sentence, obj['entity']):
-            numbers = stanford_normaliser(sentence)
+         #   numbers = stanford_normaliser(sentence)
 
             date_positions = []
             number_positions = []
@@ -72,7 +83,13 @@ def find_utterances_for_tuple(lines, obj, relation_match_strategy=exact_or_fuzzy
                     date_positions.append(i)
                 elif ne_tag in number_ne_types and number_val is not None:
                     number_positions.append(i)
-            print(number_positions)
+
+
+            match = Match(sentence,number_positions,date_positions,coref_entity_positions,entity_positions)
+            match.get_features()
+
+
+
 
     possible_matches = set(possible_matches)
     return possible_matches
@@ -95,7 +112,7 @@ if __name__ == "__main__":
     for line in open("data/distant_supervision/scraped_texts/0c5988c0ec2256d525f0d648d01a9fa56e614a30ee03dcbcc22168039b603c0d.txt","r"):
         passage.append(line.strip())
 
-    passage.append("There were 2,000,000.53 dollars in the 19th prize fund")
+    passage.append("Mečíř received three thousand dollars and there were 2,000,000.53 dollars in the 19th prize fund")
 
 
     matches = find_utterances_for_tuple(passage, {"entity":"Mečíř","relation":"improved further in","target_":1985})
